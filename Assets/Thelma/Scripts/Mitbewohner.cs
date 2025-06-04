76c86bdd635ec7b3a412597d1714e02d08627f8a
thel-ma2 <ttetteh@stud.macromedia.de>
2025-06-04 15:13:53 +0200

    Update Frühstück Minigame
    - Kamera Winkel und Einstellungen
    - Objekte und Wände haben jetzt funktionierende Collider
    - 4 Mitbewohner, die selbstständig herumlaufen und mit dem Player interagieren können
    - Inputsystem für die Steuerung des Players

---
+++ Assets/Thelma/Scripts/Mitbewohner.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.LightTransport;
+using UnityEngine.TestTools;
+
+public class Mitbewohner : MonoBehaviour
+{
+    SphereCollider myTrigger;
+
+    private bool conversation = false;
+
+
+    //Components
+    NavMeshAgent myAgent;
+    public float moveRange = 500;
+    public float chanceToStandAround = 10;
+    private bool isStandingAround;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myTrigger = GetComponent<SphereCollider>();
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isStandingAround == false)
+        {
+            RandomWalk();
+        }
+    }
+
+    void RandomWalk()
+    {
+        //Return if already moving
+        if (myAgent.pathPending || !myAgent.isOnNavMesh || myAgent.remainingDistance > 0.1f)
+            return;
+
+        if (Random.Range(0, 100) < chanceToStandAround)
+        {
+            //Stand Around
+            StartCoroutine(StandAround());
+        }
+        else //Walk around
+        {
+            WalkAround();
+        }
+    }
+
+    void WalkAround()
+    {
+        //Get Random Point and move there
+        Vector3 randomDestination = GetRandomNavMeshPoint(transform.position, moveRange);
+        myAgent.SetDestination(randomDestination);
+    }
+    IEnumerator StandAround()
+    {
+        isStandingAround = true;
+
+        float idleTime = Random.Range(1f, 3f);
+        yield return new WaitForSeconds(idleTime);
+
+        myTrigger.enabled = true;
+
+        isStandingAround = false;
+    }
+
+    Vector3 GetRandomNavMeshPoint(Vector3 center, float range, int maxTries = 10)
+    {
+        for (int i = 0; i < maxTries; i++)
+        {
+            // Pick a random point in a circle around the center
+            Vector2 circle = Random.insideUnitCircle * range;
+            Vector3 randomPoint = center + new Vector3(circle.x, 0, circle.y);
+
+            // Try to find the nearest NavMesh point to that location
+            if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 1.5f, NavMesh.AllAreas))
+            {
+                return hit.position;
+            }
+        }
+
+        // Fallback to current position if no valid point was found
+        return center;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && conversation == false)
+        {
+            if (collider.gameObject.GetComponent<Player>().conversation == false)
+                StartConversation(collider.gameObject);
+        }
+    }
+
+    void StartConversation(GameObject player)
+    {
+        isStandingAround = true;
+        conversation = true;
+        player.GetComponent<Player>().StartConversation(gameObject);
+        myAgent.SetDestination(transform.position);
+        myTrigger.enabled = false;
+    }
+
+    public void EndConversation()
+    {
+        conversation = false;
+
+        StartCoroutine(StandAround());
+    }
+
+}
---
+++ Assets/Thelma/Scripts/Player.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // grab/ take > sammeln + an Ziel ablegen
+    // eat
+
+    //Input System
+    private InputSystem_Actions playerControls;
+    private Vector2 movementInput;
+    // private Vector2 lookInput;
+
+    private GameObject currentMitbewohner;
+
+    private CharacterController myCharacterController;
+    private AudioSource audioSource;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip walkingSound;
+    [SerializeField] private AudioClip cookingSound;
+    [SerializeField] private AudioClip eatingSound;
+    [SerializeField] private AudioClip talkingSound;
+
+    [Header("Numbers")]
+    private int kPressCount = 0;
+    [SerializeField] private int requiredKPressCount = 5;
+    [SerializeField] private float moveSpeed = 2f;
+    public bool conversation = false;
+
+    private void Awake()
+    {
+        playerControls = new InputSystem_Actions();
+        myCharacterController = GetComponent<CharacterController>();
+
+        playerControls.Enable();
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        playerControls.Player.Move.performed += OnMovementPerformed;
+        playerControls.Player.Move.canceled += OnMovementCanceled;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext context)
+    {
+        // Read the movement value when the action is performed
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCanceled(InputAction.CallbackContext context)
+    {
+        // Reset movement when the action is canceled
+        movementInput = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (conversation == false)
+            Move();
+
+        if (Input.GetKeyDown("k"))
+        {
+            kPressCount++;
+
+            if (kPressCount >= requiredKPressCount)
+                EndConversation();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 moveDirection = new Vector3(movementInput.x, 0, movementInput.y);
+
+        myCharacterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        /*
+        if (!audioSource.isPlaying && movementInput != Vector2.zero)
+        {
+            audioSource.PlayOneShot(walkingSound);
+            Debug.Log("tap tap tap ...");
+        }
+        */
+    }
+
+    void EndConversation()
+    {
+        currentMitbewohner.GetComponent<Mitbewohner>().EndConversation();
+        conversation = false;
+        Debug.Log("Conversation ended, player can continue.");
+    }
+
+    public void StartConversation(GameObject mitbewohner)     // wird vom Mitbewohner aufgerufen
+    {
+        currentMitbewohner = mitbewohner;
+        conversation = true;
+        Debug.Log("Entered a Conversation");
+    }
+
+}
