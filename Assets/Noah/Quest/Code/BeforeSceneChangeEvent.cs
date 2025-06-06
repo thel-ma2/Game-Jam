ae6eec0bd9b8f9032c2ec9a33ab3b42b7480e1bf
NoahLecker <ndiazaguilera@stud.macromedia.de>
2025-06-06 14:34:16 +0200

    AlteQuesrSystem entfernt

--- Assets/Noah/Quest/Code/BeforeSceneChangeEvent.cs
+++
@@ -1,9 +0,0 @@
-public class BeforeSceneChangeEvent
-{
-    public string NextSceneName { get; private set; }
-
-    public BeforeSceneChangeEvent(string nextSceneName)
-    {
-        NextSceneName = nextSceneName;
-    }
-}
